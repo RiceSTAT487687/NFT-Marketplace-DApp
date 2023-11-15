d77b3d3cd11882d0cbbf0452ccbe71e774577a5f
James Aman <james@aman.ws>
2023-11-15 09:55:57 -0600

    Update networkAddress.js

--- frontend/config/networkAddress.js
+++ frontend/config/networkAddress.js
@@ -1,3 +1,3 @@
 export const nftMarketplaceAddress =
   "0x358082C2F8197e948836bF0d86cD8B52A56BE868";
-export const nftAddress = "0x92ea250a4A0319470B98EA8e29da1cF8714d10Fa";
+export const nftAddress = "0xc8AdfB46894e31AEe5212f3E51172EFcB3ecBf98";
